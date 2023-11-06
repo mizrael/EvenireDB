53ea1c006540fe4a20a3b3a3fc3ef12d18118d09
David Guida <obiuan@gmail.com>
2023-11-06 13:12:39 -0500

    added todo

--- src/EvenireDB.Client/IEventsClient.cs
+++ src/EvenireDB.Client/IEventsClient.cs
@@ -4,6 +4,7 @@ namespace EvenireDB.Client
 {
     public interface IEventsClient
     {
+        //TODO: add response
         ValueTask AppendAsync(Guid streamId, IEnumerable<Event> events, CancellationToken cancellationToken = default);
 
         IAsyncEnumerable<Event> ReadAsync(Guid streamId, StreamPosition position, Direction direction = Direction.Forward, CancellationToken cancellationToken = default);
