b1d232892fe8fda2befb8bb1a161db5c868ed711
David Guida <1432872+mizrael@users.noreply.github.com>
2024-06-07 16:37:01 -0400

    minor fixes

--- src/tools/EvenireDB.Tools.EventsGenerator/Program.cs
+++ src/tools/EvenireDB.Tools.EventsGenerator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using EvenireDB.Client;
 using Microsoft.Extensions.DependencyInjection;
 using System.CommandLine;
+using System.Text;
 
 var defaultServerUri = new Uri("http://localhost:5001");
 var defaultEventsCount = 100;
@@ -18,7 +19,7 @@ var eventsCount = new Option<int>(
               description: $"Count of events to send. Defaults to {defaultEventsCount}.");
 
 var streamIdOption = new Option<Guid>(
-              name: "--stream",                            
+              name: "--stream",
               description: "The stream to use.");
 
 
@@ -29,7 +30,7 @@ var rootCommand = new RootCommand
     eventsCount,
     streamIdOption
 };
-rootCommand.SetHandler(async (streamId, uri, useGrpc, eventsCount) => {     
+rootCommand.SetHandler(async (streamId, uri, useGrpc, eventsCount) => {
     var services = new ServiceCollection();
     services.AddEvenireDB(new EvenireConfig(uri, useGrpc));
 
@@ -38,7 +39,8 @@ rootCommand.SetHandler(async (streamId, uri, useGrpc, eventsCount) => {
 
     Console.ForegroundColor = ConsoleColor.Yellow;
     Console.WriteLine($"Sending {eventsCount} events to stream '{streamId}' on server '{uri}'...");
-    var events = Enumerable.Range(0, eventsCount).Select(i => new EventData($"event-{i}", new byte[] { 42 })).ToArray();
+
+    var events = Enumerable.Range(0, eventsCount).Select(i => new EventData($"event-{i}", Encoding.UTF8.GetBytes($"event-{i}"))).ToArray();
 
     try
     {
@@ -48,9 +50,9 @@ rootCommand.SetHandler(async (streamId, uri, useGrpc, eventsCount) => {
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("an error has occurred while sending events: " + cliEx.Message);
-    }    
+    }
 
-    Console.ForegroundColor = ConsoleColor.Green;    
+    Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("Done.");
 
     Console.ResetColor();
