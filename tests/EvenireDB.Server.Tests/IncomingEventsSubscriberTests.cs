8de695c8dbef2274d9e316f011e080c424f1ce3c
David Guida <1432872+mizrael@users.noreply.github.com>
2024-06-20 10:45:18 -0400

    minor fixes

--- tests/EvenireDB.Server.Tests/IncomingEventsSubscriberTests.cs
+++ tests/EvenireDB.Server.Tests/IncomingEventsSubscriberTests.cs
@@ -10,27 +10,44 @@ public class IncomingEventsSubscriberTests
     [Fact]
     public async Task Service_should_handle_exceptions_gracefully()
     {
-        var channel = Channel.CreateBounded<IncomingEventsBatch>(10);
+        var events = Array.Empty<Event>();
+        var streamId = new StreamId(Guid.NewGuid(), "lorem");
+        var expectedGroups = Enumerable.Range(0, 10)
+            .Select(i => new IncomingEventsBatch(streamId, events))
+            .ToArray();
+
+        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+        int batchIndex = 0;
+
+        var reader = Substitute.ForPartsOf<ChannelReader<IncomingEventsBatch>>();
+        reader.WaitToReadAsync(Arg.Any<CancellationToken>())
+            .ReturnsForAnyArgs(ValueTask.FromResult(batchIndex < expectedGroups.Length));
+        reader.TryRead(out Arg.Any<IncomingEventsBatch>())
+            .Returns(x =>
+            {
+                if (batchIndex >= expectedGroups.Length)
+                {
+                    cts.Cancel();
+                    return false;
+                }
+
+                x[0] = expectedGroups[batchIndex++];
+                return true;
+            });
+
         var repo = Substitute.For<IEventsProvider>();
-        repo.WhenForAnyArgs(r => r.AppendAsync(Arg.Any<StreamId>(), null, default))
+        repo.WhenForAnyArgs(r => r.AppendAsync(streamId, events, Arg.Any<CancellationToken>()))
             .Throw<Exception>();
 
         var logger = Substitute.For<ILogger<IncomingEventsPersistenceWorker>>();
 
-        var sut = new IncomingEventsPersistenceWorker(channel.Reader, repo, logger);
+        var sut = new IncomingEventsPersistenceWorker(reader, repo, logger);        
+        
+        await sut.StartAsync(cts.Token);
 
-        await sut.StartAsync(default);
-
-        var groups = Enumerable.Range(0, 10)
-            .Select(i => new IncomingEventsBatch(Arg.Any<StreamId>(), null))
-            .ToArray();
-        foreach (var group in groups)
-            await channel.Writer.WriteAsync(group);
-
-        await Task.Delay(2000);
-        await sut.StopAsync(default);
-
-        await repo.ReceivedWithAnyArgs(groups.Length)
-                  .AppendAsync(Arg.Any<StreamId>(), null, default);
+        await Task.Delay(TimeSpan.FromSeconds(2));
+        
+        await repo.ReceivedWithAnyArgs(expectedGroups.Length)
+                  .AppendAsync(Arg.Any<StreamId>(), Arg.Any<IEnumerable<Event>>(), default);
     }
 }
